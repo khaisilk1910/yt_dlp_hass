4ad066ab243b288d265d5b0ef38d42340b2a13ab
khaisilk1910 <3167468+khaisilk1910@users.noreply.github.com>
2026-08-26 22:58:59 +0700

    Add files via upload

--- custom_components/yt_dlp/const.py
+++ custom_components/yt_dlp/const.py
@@ -3,7 +3,7 @@
 from __future__ import annotations
 
 DOMAIN = "yt_dlp"
-VERSION = "0.5.26"
+VERSION = "0.5.27"
 
 STATE_DOWNLOADER = f"{DOMAIN}.downloader"
 STATE_FAVORITES_PLAYBACK = f"{DOMAIN}.favorites_playback"
